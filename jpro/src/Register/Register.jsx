3becaf8f7feacc2208ef14ab933ea8e99e250358
pius-code <obliepius13@gmail.com>
2024-10-22 16:56:56 +0000

    sdfs

--- jpro/src/Register/Register.jsx
+++ jpro/src/Register/Register.jsx
@@ -62,16 +62,13 @@ const Register = () => {
 
     // If validation passes, make the API call
     try {
-      const response = await axios.post(
-        "https://jback.vercel.app/register/register",
-        {
-          fullName,
-          Number,
-          Email,
-          Password,
-          Location,
-        }
-      );
+      const response = await axios.post("https://jback.vercel.app/register", {
+        fullName,
+        Number,
+        Email,
+        Password,
+        Location,
+      });
       setColor("Green");
       setFillSpaces(`${response.data}, Redirecting to login page...`);
       setTimeout(() => {
